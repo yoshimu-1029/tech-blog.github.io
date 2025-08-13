546c7eed72adcc948e8af158609da5b0c64d5652
Kana.Y <kanae.yoshimura27@gmail.com>
2025-08-13 15:12:01 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://yoshimu-1029.github.io/tech-blog.github.io/";
